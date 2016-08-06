1b36410cdcbd0a4a28090f116b61173df626cb80
TickleThePanda <thomas.pg.attwood@gmail.com>
2016-08-06 22:18:59 +0100

    adding xresources to dotfiles

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-ln -sfv "$DOTFILES_DIR/runcom/.bash_aliases" ~
-ln -sfv "$DOTFILES_DIR/runcom/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/runcom/.bashrc" ~
-ln -sfv "$DOTFILES_DIR/runcom/.env" ~
-ln -sfv "$DOTFILES_DIR/runcom/.prompt" ~
+ln -sfv "$DOTFILES_DIR/runcom/bash_aliases" ~/.bash_aliases
+ln -sfv "$DOTFILES_DIR/runcom/bash_profile" ~/.bash_profile
+ln -sfv "$DOTFILES_DIR/runcom/bashrc" ~/.bashrc
+ln -sfv "$DOTFILES_DIR/runcom/env" ~/.env
+ln -sfv "$DOTFILES_DIR/runcom/prompt" ~/.prompt
+
+ln -sfv "$DOTFILES_DIR/Xresources/Xresources" ~/.Xresources
+
