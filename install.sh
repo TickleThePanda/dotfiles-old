1729be4dacf1d4ee4048ae40531dcd88d87de576
TickleThePanda <thomas.pg.attwood@gmail.com>
2016-08-07 13:57:42 +0100

    bash prompt and style

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
+#!/bin/bash
+
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# install prerequisites
+
+git clone git@github.com:magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+
+# linking files
 
 ln -sfv "$DOTFILES_DIR/runcom/bash_aliases" ~/.bash_aliases
 ln -sfv "$DOTFILES_DIR/runcom/bash_profile" ~/.bash_profile
