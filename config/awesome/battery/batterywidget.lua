9988775b3881d6419cafdbb7fbee72a6be571f2b
TickleThePanda <thomas.pg.attwood@gmail.com>
2016-08-22 19:40:36 +0100

    add battery widget for awesome!

--- config/awesome/battery/batterywidget.lua
+++ config/awesome/battery/batterywidget.lua
@@ -4,39 +4,53 @@ batterywidget = wibox.widget.textbox()
 batterywidget.WARNING_CUTOFF = 15
 batterywidget.loaded_text = "00% ~00:00"
 
-batterywidget.get_text = function()
-  fh = assert(io.popen("acpi | cut -d ' ' -f 4,5 | cut -d ':' -f -2 -", "r"))
-  text = fh:read("*l"):gsub(",", ""):gsub(" ", " ~")
+batterywidget.load_values = function()
+  fh = assert(io.popen("acpi", "r"))
+  text = fh:read("*l")
   fh.close()
-  return text
+  useful_text = text:gsub(".*: ",""):gsub("remaining", ""):gsub(":[0-9]* ", ""):gsub("%%, ", "%% ~")
+  view_text = useful_text:gsub("^[a-zA-Z]*, ", "")
+
+  batterywidget.battery_status = useful_text:gsub(",.*", "")
+  batterywidget.loaded_text = view_text
+  batterywidget.battery_percent = view_text:gsub("%%.*", "")
 end
 
-batterywidget.loaded_text = batterywidget.get_text()
+batterywidget.load_values()
 
 batterywidget.load_timer = timer({ timeout = 5 })
 batterywidget.load_timer:connect_signal("timeout",    
   function()
-    batterywidget.loaded_text = batterywidget.get_text()
+    batterywidget.load_values()
   end    
-)    
+) 
 batterywidget.load_timer:start()
 
 batterywidget.flashed = false
 
-batterywidget.refresh_timer = timer({ timeout = 1})
+batterywidget.refresh_timer = timer({ timeout = 1 })
 batterywidget.refresh_timer:connect_signal("timeout",
   function()
-    battery_percent = batterywidget.loaded_text:gsub("%%.*", "")
-
-    coloured_text = batterywidget.loaded_text
-    if tonumber(battery_percent) < batterywidget.WARNING_CUTOFF then
-        batterywidget.flashed = not batterywidget.flashed
-	if batterywidget.flashed then
-           coloured_text = "<span color=\"red\">" .. coloured_text .. "</span>"
-        end
+    color_text = function(input_text, color)
+      return "<span color = \"" .. color .."\">" .. input_text .. "</span>"
     end
 
-    batterywidget:set_markup(" | " .. coloured_text  .. " | ")    
+    battery_percent = batterywidget.battery_percent
+    battery_status = batterywidget.battery_status
+    colored_text = batterywidget.loaded_text
+
+    if battery_status == "Discharging" then
+      if tonumber(battery_percent) < batterywidget.WARNING_CUTOFF then
+        batterywidget.flashed = not batterywidget.flashed
+        if batterywidget.flashed then
+          colored_text = color_text(colored_text, "red")
+        end
+      end
+    else
+      colored_text = color_text(colored_text, "green")
+    end
+
+    batterywidget:set_markup(" | " .. colored_text  .. " | ")    
   end
 )
 batterywidget.refresh_timer:start()
