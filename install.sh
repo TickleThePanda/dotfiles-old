ed46b0aface76d2174a67110dcbf44a8841beb59
Thomas Attwood <thomas.attwood@stfc.ac.uk>
2016-10-05 11:50:33 +0100

    updating config

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # install prerequisites
 
+
 git clone git@github.com:magicmonty/bash-git-prompt.git ~/.bash-git-prompt --depth=1
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 wget -O ~/.git-completion https://raw.githubusercontent.com/git/git/master/contrib/completion/git-completion.bash
 
@@ -17,6 +20,8 @@ ln -sfv "$DOTFILES_DIR/runcom/env" ~/.env
 ln -sfv "$DOTFILES_DIR/runcom/prompt" ~/.prompt
 
 ln -sfv "$DOTFILES_DIR/Xresources/Xresources" ~/.Xresources
+
+mkdir "$DOTFILES_DIR/.config"
 
 ln -sfv "$DOTFILES_DIR/config/awesome" ~/.config/
 
